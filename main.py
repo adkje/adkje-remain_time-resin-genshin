89718a5e2243697910903c46bd0a1151c9141d90
abkje <abcdeabcde.123.123.456@gmail.com>
2025-03-14 13:52:19 +0900

    classを分割したいけどこれ無理だ！！！

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import ActiveDisco
 url = "https://bbs-api-os.hoyoverse.com/game_record/genshin/api/dailyNote"
 # This URL provides the most information compared to the URLs above, but you must enter a UID of Genshin.
 
-# Classを使う意味はありませんが直すのが面倒なので使ったまま！！
-# APIの送信と待機時間の計算
+# これclassを使う意味ある？
+# APIの送信
 class User:
     # APIに必要な情報の格納
     def __init__(self,wait_time,name,cookie_token,ltoken,ltuid,uid):
@@ -39,53 +39,68 @@ class User:
                           "schedule_type": 1,
                         }
         
-    # 
-    async def all_process(self):
-        print("all_process")
-        await asyncio.sleep(10)
-        # クソみたいな実装方法だからいつか直す。多分。恐らく。
-        # botが起動してSendUserが定義される前に下の処理が走っちゃうからOverOneHundredEighy()でエラーが出る。
-        while True:
-            response = requests.get(url, cookies = self.cookies, params = self.params).json()
-            self.resin_time = int(response["data"]["resin_recovery_time"])
+    # 違う処理を作るにあたってAPI送信=計算となるのは良くないと感じた。
+    async def Send_API(self):
+        print("SendAPI")
+        self.Receive_response = requests.get(url, cookies = self.cookies, params = self.params).json()
+        self.Receive_resin_time = int(self.Receive_response["data"]["resin_recovery_time"])
+        global ohayou
+        ohayou = self.Receive_resin_time
+        return ohayou
+
+async def keisan(keisan_resin_time):
+
+    keisan_wait_time = keisan_resin_time % 480
+    #  次の樹脂までの秒数(60秒*8で八分)。sleep時間に使用。
+    print(f"待つ秒数は{keisan_wait_time}")
             
-            print(f"残り秒数は{self.resin_time}")
+    Max_minute = math.ceil(keisan_resin_time / 60)
+    #   樹脂が200になるまでの分数。メッセージに載せる用。
+    print(f"残り分数は{Max_minute}")
+    
 
-            self.wait_time = self.resin_time % 480
-            #  次の樹脂までの秒数(60秒*8で八分)。sleep時間に使用。
             
-            self.Max_minute = math.ceil(self.resin_time / 60)
-            #   樹脂が200になるまでの分数。メッセージに載せる用。
-            print(f"残り分数は{self.Max_minute}")
-            
+async def okurimasu(keisan_resin_time,Max_minute,keisan_wait_time):
+    if keisan_resin_time != 0:
+    # if 100000 > self.resin_time:
+    # デバッグ用
+        print("okurimasu")
 
-            if self.resin_time != 0:
-                print(f"待つ秒数は{self.wait_time}")
+        if 9600 > keisan_resin_time:
+        # 樹脂が180以上
+            print("mattemasu")
+            await ActiveDisco.OverOneHundredEighy(Max_minute)
+            await asyncio.sleep(keisan_wait_time)
 
-                # if 100000 > self.resin_time:
-                # デバッグ用
-
-                if 9600 > self.resin_time:
-                # 樹脂が180以上
-                    print("mattemasu")
-                    await ActiveDisco.OverOneHundredEighy(self.Max_minute)
-                    await asyncio.sleep(self.wait_time)
-
-
-            else:
-                print(self.wait_time)
-                await ActiveDisco.caveat()
-                await asyncio.sleep(1200)
-                # 秘境の樹脂消費量に合わせた20樹脂(60*20)
-
+    else:
+        print(keisan_wait_time)
+        await ActiveDisco.caveat()
+        await asyncio.sleep(1200)
+        # 秘境の樹脂消費量に合わせた20樹脂(60*20)
 
 
 User1 = User(0,"User1",Glist.User1_cookie_token,Glist.User1_ltoken,Glist.User1_ltuid,Glist.User1_uid)
 # User2 = User(0,"User2",Glist.User1_cookie_token,Glist.User1_ltoken,Glist.User1_ltuid,Glist.User1_uid)
 
 Alluser = [User1]  
-async def AllBoot():
-    await asyncio.gather(ActiveDisco.BootBot(),*(NowUser.all_process() for NowUser in Alluser))
+# ここまでは変える必要はない。
+async def aiueo():
+    await User.Send_API()
+    await keisan(User.Send_API())
+    await okurimasu()
+    
 
-asyncio.run(AllBoot())
+    # await asyncio.gather(ActiveDisco.BootBot(),*(NowUser.hello() for NowUser in Alluser))
+
+# async def AutoSend():
+#     while True:
+#         await asyncio.gather(ActiveDisco.BootBot(),aiueo())
+#         keisan(ohayou)
+#         okurimasu()
+async def main():
+    await asyncio.gather(ActiveDisco.BootBot(),aiueo())
+# async def AllBoot():
+#     await asyncio.gather(ActiveDisco.BootBot(),*(NowUser.all_process() for NowUser in Alluser))
+
+asyncio.run(main())
 
